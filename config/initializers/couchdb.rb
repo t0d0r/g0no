fac06ecb139fd553fec052cb13bc14434d7b4935
Todor Dragnev <todor.dragnev@gmail.com>
2011-05-12 03:49:36 +0300

    adding support for config/couchdb.yml

--- config/initializers/couchdb.rb
+++ config/initializers/couchdb.rb
@@ -1,4 +1,11 @@
 require 'couchrest/model'
-#CouchServer = CouchRest::Server.new "http://admin:alabala@127.0.0.1:5984"# defaults to localhost:5984
-CouchServer = CouchRest::Server.new "http://127.0.0.1:5984"# defaults to localhost:5984
+COUCHDB = YAML::load(ERB.new(IO.read("#{Rails.root.to_s}/config/couchdb.yml")).result)[Rails.env]
+#YAML::load(ERB.new(IO.read(xmlrpc_configuration_file)).result) [ENV['G00NET_ENV']]
+CouchServer = CouchRest::Server.new "http://#{COUCHDB['server']}"
+if COUCHDB["required_authentination"]
+  CouchServer = CouchRest::Server.new "http://#{COUCHDB['username']}:#{COUCHDB['password']}@#{COUCHDB['server']}"
+else
+  CouchServer = CouchRest::Server.new "http://#{COUCHDB['server']}"
+end
+
 CouchServer.default_database = "gono-#{Rails.env}"
